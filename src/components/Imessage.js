5c65277ed66b89ed0b8de308c54eca65e959eee1
ufuk <ufuk.uysl5@gmail.com>
2020-10-16 13:20:29 +0200

    Create message component

--- src/components/Imessage.js
+++ src/components/Imessage.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import Chat from './Chat'
 import "./Imessage.css"
 import Sidebar from './Sidebar'
 function Imessage() {
     return (
         <div className="imessage">
             <Sidebar />
+            <Chat />
         </div>
     )
 }
