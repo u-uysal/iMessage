bcd7a5b36f67f79b3e3fe1166f937edbd7f084ed
ufuk <ufuk.uysl5@gmail.com>
2020-10-28 14:59:24 +0100

    get chatname info from database

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import SidebarChat from "./SidebarChat"
 import { Avatar, IconButton } from "@material-ui/core"
 import "./Sidebar.css"
@@ -6,28 +6,74 @@ import SearchIcon from "@material-ui/icons/Search"
 import RateReviewIcon from '@material-ui/icons/RateReview';
 import { useSelector } from 'react-redux'
 import { selectUser } from '../features/userSlice'
+import db, { auth } from "../firebase"
+import firebase from "firebase"
 
 function Sidebar() {
     const user = useSelector(selectUser)
-    console.log(user.photo)
+    const [chats, setChats] = useState([])
+    useEffect(() => {
+
+        const downloadChatName = firebase.database().ref("chats");
+        downloadChatName.on("value", (snapshot) => {
+            const allName = snapshot.val()
+            const nameList = [];
+            for (let id in allName) {
+                nameList.push({ id, ...allName[id] });
+            }
+
+            setChats(nameList)
+
+        })
+
+
+
+
+
+        /* db.collection("chats").onSnapshot(snapshot => (
+            setChats(snapshot.docs.map(doc => ({
+                id: doc.id,
+                data: doc.data()
+            })))
+        )) */
+    }, [])
+
+    const addChat = () => {
+        const chatName = prompt("Please enter a chat name");
+
+        if (chatName) {
+            const uploadMessage = firebase.database().ref("chats");
+            const addChatName = {
+                chatName: chatName,
+            }
+
+            uploadMessage.push(addChatName);
+            /*    db.collection("chats").add({
+                   chatName: chatName,
+               }) */
+        } else {
+            window.location.reload()
+        }
+
+    }
+
     return (
         <div className='sidebar'>
 
             <div className="sidebar__header">
-                <Avatar src={user.photo} className='sidebar__avatar' />
+                <Avatar onClick={() => auth.signOut()} src={user.photo} className='sidebar__avatar' />
                 <div className="sidebar__input">
                     <SearchIcon />
                     <input placeholder="search" />
                 </div>
-                <IconButton variant='outlined' className="sidebar__inputButton"><RateReviewIcon /></IconButton>
+                <IconButton variant='outlined' className="sidebar__inputButton"><RateReviewIcon onClick={addChat} /></IconButton>
             </div>
             <div className="sidebar__chat">
-                <SidebarChat />
-                <SidebarChat />
-                <SidebarChat />
-                <SidebarChat />
-                <SidebarChat />
-                <SidebarChat />
+                {chats.map((chat) => (
+                    <SidebarChat key={chat.id} id={chat.id} chatName={chat.chatName} />
+                ))}
+
+
             </div>
         </div>
     )
--- src/components/SidebarChat.js
+++ src/components/SidebarChat.js
@@ -2,12 +2,12 @@ import React from 'react'
 import "./SidebarChat.css"
 import { Avatar } from "@material-ui/core"
 
-function SidebarChat() {
+function SidebarChat({ id, chatName }) {
     return (
         <div className="sidebarChat">
             <Avatar />
             <div className="sidebarChat__info">
-                <h3>Channel Name</h3>
+                <h3>{chatName}</h3>
                 <p>Last Message sent</p>
                 <small>timestamp</small>
             </div>
